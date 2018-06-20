a67435700e6d60567d13de856c8b88ad2c5a3f31
tylerturnbull <tsquarredapplications@gmail.com>
2018-06-20 12:23:37 -0400

    renamed MainActivity to LoginActivity

--- android/BAB/app/src/main/java/bab/com/build_a_bridge/LoginActivity.kt
+++ android/BAB/app/src/main/java/bab/com/build_a_bridge/LoginActivity.kt
@@ -3,7 +3,7 @@ package bab.com.build_a_bridge
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
