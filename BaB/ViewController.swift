bc16ba3f8ee93e76d9c888640c3c767f5a883eaf
Baily Troyer <bailytroyer@Bailys-MacBook-Pro.local>
2018-06-19 20:29:38 -0400

    Initial Commit

---
+++ BaB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BaB
+//
+//  Created by Baily Troyer on 6/19/18.
+//  Copyright © 2018 Build-A-Bridge-Foundation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
