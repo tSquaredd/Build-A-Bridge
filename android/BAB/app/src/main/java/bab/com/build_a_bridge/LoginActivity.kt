0fff3ba4febdf66f23f4ffc5167b8ceed20e5850
tylerturnbull <tsquarredapplications@gmail.com>
2018-07-05 18:52:14 -0400

    Starting to add firebase auth..

--- android/BAB/app/src/main/java/bab/com/build_a_bridge/LoginActivity.kt
+++ android/BAB/app/src/main/java/bab/com/build_a_bridge/LoginActivity.kt
@@ -3,21 +3,27 @@ package bab.com.build_a_bridge
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
-
-
-
-
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        auth = FirebaseAuth.getInstance()
+    }
 
-
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            // user is signed in
+        }
     }
 
 
