8496b33ff6d20121fcaf795fc7a4724d0af788e4
Baily Troyer <bailytroyer@Bailys-MacBook-Pro.local>
2018-07-11 22:31:51 -0400

    Email auth

---
+++ iOS/Firebase_iOS/BaBLogic/ViewControllerSignUpTasking.swift
@@ -0,0 +1,35 @@
+//
+//  ViewControllerSignUpTasking.swift
+//  BaBLogic
+//
+//  Created by Baily Troyer on 7/8/18.
+//  Copyright © 2018 Build-A-Bridge-Foundation. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerSignUpTasking: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
