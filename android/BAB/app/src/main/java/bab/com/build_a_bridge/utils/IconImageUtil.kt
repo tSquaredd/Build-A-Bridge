0189aa999b6ace48f97866d394b1604946cea72e
tylerturnbull <tsquarredapplications@gmail.com>
2018-08-01 19:09:02 -0400

    Basic request data loading into RequestDetailsFragment

---
+++ android/BAB/app/src/main/java/bab/com/build_a_bridge/utils/IconImageUtil.kt
@@ -0,0 +1,57 @@
+package bab.com.build_a_bridge.utils
+
+import android.content.Context
+import android.content.ContextWrapper
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.preference.PreferenceManager
+import bab.com.build_a_bridge.enums.PreferenceNames
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+
+class IconImageUtil {
+    companion object {
+        const val PHOTO_DIRECTORY = "build_a_bridge_skill_icons"
+
+        fun savePhoto(context: Context, bitmap: Bitmap, skillId: String): String{
+            val contextWrapper = ContextWrapper(context)
+            val fileDirectory = contextWrapper.getDir(PHOTO_DIRECTORY, Context.MODE_PRIVATE)
+            val filePath = File(fileDirectory, skillId)
+
+            var fileOutputStream: FileOutputStream? = null
+
+            try {
+                fileOutputStream = FileOutputStream(filePath)
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream)
+            } catch (e: Exception){
+                e.printStackTrace()
+            } finally {
+                fileOutputStream?.close()
+            }
+
+            return fileDirectory.absolutePath
+        }
+
+        fun loadPhotoFromInternalStorage(context: Context, skillId: String): Bitmap?{
+            var bitmap: Bitmap? = null
+            try {
+                val directory = context.filesDir
+                val file = File(directory, skillId)
+                bitmap = BitmapFactory.decodeStream(FileInputStream(file))
+            } catch (e: FileNotFoundException){
+                e.printStackTrace()
+            }
+
+            return bitmap
+        }
+
+        fun removePhoto(context: Context, skillId: String){
+            val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+            val filePath = prefs.getString(PreferenceNames.PROFILE_PICTURE.toString(), "null")
+            val file = File(filePath, skillId)
+            file.delete()
+        }
+    }
+}
